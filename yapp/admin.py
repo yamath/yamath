c62a9904b5fb28c5a6942165f950eaf0f8616ef5
yamatteo <yamatteo@gmail.com>
2017-02-25 17:44:06 -0500

    ...

--- yapp/admin.py
+++ yapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Topic, TopicDependency, Question, Answer, Score
+
+admin.site.register(Topic)
+admin.site.register(TopicDependency)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Score)
