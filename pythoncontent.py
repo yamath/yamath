c31e218a823f0eae7057d90e18978877bb32e67a
Matteo Bortolotto <mbortolotto@raimondi.edu.pe>
2017-03-29 22:53:37 +0000

    sfgsdfgdf

---
+++ pythoncontent.py
@@ -0,0 +1,64 @@
+mylist([
+    mydict({
+        'serial':'Q01',
+        'name':'Scrittura posizionale: numeri naturali',
+        'topics':mylist([
+            mydict({
+                'questions':mylist([
+                    mydict({
+                        'text':'Usiamo comunemente un sistema di numerazione decimale posizionale.',
+                        'kind':'b',
+                        'serial':'01',
+                        'options':mylist([
+                            mydict({
+                                'text':'vero',
+                                'accepted':True,
+                            }),
+                        ]),
+                    }),
+                    mydict({
+                        'text':'Esiste soltanto il sistema numerico decimale.',
+                        'kind':'b',
+                        'serial':'02',
+                        'options':mylist([
+                            mydict({
+                                'text':'falso',
+                                'accepted':True,
+                            }),
+                        ]),
+                    }),
+                    mydict({
+                        'text':'Tutti i sistemi di numerazione sono posizionali, alcuni però non sono decimali.',
+                        'kind':'b',
+                        'serial':'03',
+                        'options':mylist([
+                            mydict({
+                                'text':'falso',
+                                'accepted':True,
+                            }),
+                        ]),
+                    }),
+                ]),
+                'mobile':True,
+                'kind':'2',
+                'serial':'01',
+                'text':'Usiamo un sistema di numerazione decimale posizionale.',
+            }),
+            mydict({
+                'text':'Il nostro sistema di numerazione è detto decimale perché usa dieci simboli detti cifre.',
+                'mobile':True,
+                'kind':'2',
+                'serial':'02',
+            }),
+        ]),
+        'antes':[],
+    }),
+    mydict({
+        'serial':'Q02',
+        'name':'Scrittura posizionale: numeri decimali',
+        'topics':mylist([
+
+        ]),
+        'antes':['Q01'],
+    }),
+])
