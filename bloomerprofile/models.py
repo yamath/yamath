393d967add4ff9d6747dadea437eb488e5138978
Matteo Bortolotto <mbortolotto@raimondi.edu.pe>
2017-03-30 19:15:48 +0000

    dfhdfj

--- bloomerprofile/models.py
+++ bloomerprofile/models.py
@@ -1,3 +1,86 @@
 from django.db import models
+from django.contrib.auth.models import User
+from content.models import *
+from question.models import *
+from random import shuffle
 
-# Create your models here.
+class Bloomer(models.Model):
+    class Meta:
+        proxy = True
+
+    def __repr__(self):
+        return self.user.username
+
+    def _get_classrooms(self):
+        return [ cb.classroom for cb in ClassroomBloomer.objects.filter(bloomer=self) ]
+
+    def _get_score_mean(self):
+        return sum( self.get_scorevalue_of(t) for t in self.topics )/len(self.topics)
+
+    def _get_topics(self):
+        return list({ topic for classroom in self.classrooms for topic in classroom.topics })
+
+    def get_scorevalue_of(self, topic):
+        try:
+            return Score.objects.get(user=self.user, topic=topic).value
+        except:
+            return 0
+
+    def add_scorevalue_of(self, topic, i):
+        try:
+            score = Score.objects.get(user=self.user, topic=topic)
+        except:
+            score = Score(user=self.user, topic=topic, value=0)
+        score.value = min(100, max(0, score.value+i))
+        score.save()
+
+    def update_classrooms(self, classrooms_serial_list):
+        classrooms_to_add = set(classrooms_serial_list) - set([ c.serial for c in self.classrooms ])
+        classrooms_to_delete = set([ c.serial for c in self.classrooms ]) - set(classrooms_serial_list)
+        for serial in classrooms_to_add:
+            ClassroomBloomer(bloomer=self, classroom=Classroom.objects.get(serial=serial)).save()
+        for serial in classrooms_to_delete:
+            ClassroomBloomer.objects.get(bloomer=self, classroom=Classroom.objects.get(serial=serial)).delete()
+
+
+    classrooms = property(_get_classrooms)
+    topics = property(_get_topics)
+    score_mean = property(_get_score_mean)
+
+class Classroom(models.Model):
+    serial = models.CharField(max_length=4, unique=True)
+
+    def __str__(self):
+        return self.serial
+
+    def _get_bloomers(self):
+        return [ cb.bloomer for cb in ClassroomBloomer.objects.filter(classroom=self) ]
+
+    def _get_topics(self):
+        return list({ tc.topic for tc in TopicClassroom.objects.filter(classroom=self) })
+
+    def add_bloomer(self, bloomer):
+        if bloomer not in self.bloomers:
+            ClassroomBloomer(bloomer=bloomer, classroom=self).save()
+
+    def del_bloomer(self, bloomer):
+        if bloomer in self.bloomers:
+            ClassroomBloomer.objects.get(bloomer=bloomer, classroom=self).delete()
+
+    def add_topic(self, topic):
+        if topic not in self.topics:
+            TopicClassroom(topic=topic, classroom=self).save()
+
+    def del_topic(self, topic):
+        if topic in self.topics:
+            TopicClassroom.objects.get(topic=topic, classroom=self).delete()
+
+    bloomers = property(_get_bloomers)
+    topics = property(_get_topics)
+
+class ClassroomBloomer(models.Model):
+    bloomer = models.ForeignKey('Bloomer', related_name="classroombloomer_bloomer")
+    classroom = models.ForeignKey('Classroom', related_name="classroombloomer_classroom")
+
+    def __str__(self):
+        return "%s in %s" % (self.bloomer, self.classroom)
---
+++ content/models.py
@@ -0,0 +1,166 @@
+from django.db import models
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+from random import sample
+
+def get_or_none(_class, **kwargs):
+    try:
+        return _class.objects.get(**kwargs)
+    except ObjectDoesNotExist:
+        return None
+
+
+class Serie(models.Model):
+    serial = models.CharField(max_length=3, unique=True)
+    name = models.CharField(max_length=80)
+    recollection = models.BooleanField(default=False)
+    hindex = models.IntegerField(default=0)
+
+    def __repr__(self):
+        return "S%s" % self.serial
+
+    def __str__(self):
+        return self.name
+
+    def get_antes(self):
+        return list({ ss.ante for ss in SerieSerie.objects.filter(ante=self) })
+
+    def set_hindex(self):
+        if self.antes:
+            self.hindex = max( s.hindex for s in self.antes ) + 1
+        else:
+            self.hindex = 1
+
+    def get_posts(self):
+        return list({ ss.post for ss in SerieSerie.objects.filter(ante=self) })
+
+    def get_topics(self):
+        return list(Topic.objects.filter(serie=self))
+
+    def add_ante(self, s):
+        if isinstance(s, Serie):
+            serie = s
+        else:
+            serie = get_or_none(Serie, serial=s)
+        if serie is not None and serie not in self.antes:
+            SerieSerie(ante=serie, post=self).save()
+
+    def add_post(self, s):
+        if isinstance(s, Serie):
+            serie = s
+        else:
+            serie = get_or_none(Serie, serial=s)
+        if serie not in self.posts:
+            SerieSerie(ante=self, post=serie).save()
+
+    def del_ante(self, s):
+        if isinstance(s, Serie):
+            serie = s
+        else:
+            serie = get_or_none(Serie, serial=s)
+        if serie in self.antes:
+            SerieSerie.objects.get(ante=serie, post=self).delete()
+
+    def del_post(self, s):
+        if isinstance(s, Serie):
+            serie = s
+        else:
+            serie = get_or_none(Serie, serial=s)
+        if serie in self.posts:
+            SerieSerie.objects.get(ante=self, post=serie).delete()
+
+    antes = property(_get_antes)
+    posts = property(_get_posts)
+    topics = property(_get_topics)
+
+
+class SerieSerie(models.Model):
+    ante = models.ForeignKey('Serie', related_name='serieserie_ante')
+    post = models.ForeignKey('Serie', related_name='serieserie_post')
+
+    def __repr__(self):
+        return "SS(%s < %s)" % (self.ante.serial, self.post.serial)
+
+
+class Topic(models.Model):
+    serial = models.CharField(max_length=6, unique=True)
+    kind = models.IntegerField( blank=True, null=True,
+                                choices=(   (1, 'remembering'),
+                                            (2, 'understanding'),
+                                            (3, 'applying'),
+                                            (4, 'analyzing'),
+                                            (5, 'evaluating'),
+                                            (6, 'creating'), ))
+    mobile = models.BooleanField(default=False)
+    name = models.CharField(max_length=80)
+    serie = models.ForeignKey('Serie', related_name="topic_serie")
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return "T%s" % (self.serial)
+
+    def chk_serial(self):
+        return self.serial[0:3] == self.serie.serial
+
+    def get_questions(self):
+        return list(Question.objects.filter(topic=self))
+
+    questions = property(get_questions)
+
+
+class Question(models.Model):
+    serial = models.CharField(max_length=9, unique=True)
+    text = models.TextField()
+    topic = models.ForeignKey(Topic, related_name='question_topic')
+    kind = models.CharField(max_length=1, choices=( ('b', 'True or false'),
+                                                    ('m', 'Multiple choice'),
+                                                    ('o', 'Open question'),
+                                                    ('t', 'Teacher evaluated question'),
+                                                    ('w', 'Work to deliver to the teacher')))
+
+    def __str__(self):
+        return "%s" % self.text
+
+    def __repr__(self):
+        return "Q%s%s" % (self.kind, self.serial)
+
+    def chk_serial(self):
+        return self.serial[0:6] == self.topic.serial
+
+    def get_options(self):
+        return list(Option.objects.filter(question=self))
+
+    def get_uptofive(self):
+        correct_option = Option.objects.filter(question=self, accepted=True).first()
+        rejected_options =  Option.objects.filter(question=self, accepted=False)[0:4]
+        shuffle(corre)
+        l = l[0:4]
+        l.append(self.get_first_correct_option())
+        shuffle(l)
+        return l
+
+    def chk_answer(self, text):
+        os = Option.objects.filter(question=self, text=text)
+        if len(os)==1:
+            accepted = os.first().accepted
+        elif len(os)==0:
+            accepted = False
+        else:
+            raise MultipleObjectsReturned("Multiple options for %s" % repr(self))
+        return accepted
+
+    options = property(get_options)
+
+
+class Option(models.Model):
+    serial = models.CharField(max_length=12, unique=True)
+    question = models.ForeignKey('Question', related_name="option_question")
+    text = models.TextField()
+    accepted = models.BooleanField(default=True)
+
+    def __str__(self):
+        return "(%s) %s%s" % (repr(self.question), self.text, ' rejected' if self.accepted else '')
+
+    def __repr__(self):
+        return "O%s" % self.serial
