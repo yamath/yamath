d2a4a8103e39e5191db5d6de002200572c420528
yamatteo <yamatteo@gmail.com>
2017-04-03 20:55:20 -0500

    Double settings standard

--- push.sh
+++ push.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
+cp yamath/cloudsettings.py yamath/settings.py
 git add .
 git commit -m "$@"
 git push origin master
+cp yamath/hardcopysettings.py yamath/seyyings.py
 echo "Ricordati di fare il pull dall'altro lato!"
