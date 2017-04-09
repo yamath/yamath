ab705438f2e15fa9f0b1587a661260d68c88d69e
yamatteo <yamatteo@gmail.com>
2017-04-08 19:37:12 -0500

    Forget

--- blooming/forget.py
+++ blooming/forget.py
@@ -1,15 +1,8 @@
-from blooming.models import *
+from bloomerprofile.models import *
 from random import choice
 import logging
 
 for bloomer in Bloomer.objects.all():
-    scores = [ score for score in Score.objects.filter(user=bloomer.user) if score.value > 0 ]
-    for i in range(10):
-        try:
-            score = choice(scores)
-            print("forget %s - %s" % (bloomer.user.username, score.topic))
-            bloomer.add_scorevalue_of(score.topic, -1)
-        except:
-            pass
+    bloomer.add_forget()
 
 logging.info("Everybody forgot something")
