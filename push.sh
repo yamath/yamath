638c0af459a78531eac6ee3ae730dcb5090cdd75
yamatteo <yamatteo@gmail.com>
2017-04-30 10:22:54 -0500

    Nuovo look

--- push.sh
+++ push.sh
@@ -2,6 +2,6 @@
 cp yamath/cloudsettings.py yamath/settings.py
 git add .
 git commit -m "$@"
-git push origin master
+git push https://yamath:raimondi127@github.com/yamath/yamath.git master
 cp yamath/hardcopysettings.py yamath/settings.py
 echo "Ricordati di fare il pull dall'altro lato!"
